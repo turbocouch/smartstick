6e655a7c5254f5b466afc1c54977d05d8f45cda0
William McGonagle <wpmcgonagle@gmail.com>
2022-01-16 21:35:11 -0500

    🥳 Added New Release

--- index.ts
+++ index.ts
@@ -1,25 +1,38 @@
 import * as http from "http";
+import { URL } from "url";
 
 export default class Smartstick {
 
     server:http.Server;
+    host:string;
 
-    constructor() {
+    constructor(host) {
 
-        this.server = http.createServer(this.runServer);
+        this.host = host;
+        if (this.host == undefined) this.host = 'http://example.com';
+
+        this.server = http.createServer((req, res) => { 
+            
+            this.runServer(this, req, res) 
+        
+        });
 
     }
 
-    runServer(req, res) {
+    runServer(server, req, res) {
 
-        res.write("This is a test.");
+        let currentUrl = new URL(req.url, server.host);
+
+        console.log(currentUrl.pathname);
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(currentUrl.pathname);
         res.end();
 
     }
 
     listen(port, callback) {
 
-        
         this.server.listen(port, callback);
 
     }
